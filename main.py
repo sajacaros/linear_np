a72cbc3bac11b99e40e21cddd0e1cf2f498149b9
dukim <sajacall82@naver.com>
2023-09-27 19:18:29 +0900

    feat: NpLinearRegression 클래스 생성 및 이름 출력

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from sajacaros_learn.Regression import NpLinearRegression
+
+
 def energy_predict():
-    # np_linear_reg = NpLinearRegression()
+    np_linear_reg = NpLinearRegression()
     # np_linear_reg.fit(X, y, lr=0.1)
     # y_hat = np_linear_reg.predict(t)
     # error_report(y, y_hat)
---
+++ sajacaros_learn/Regression.py
@@ -0,0 +1,3 @@
+class NpLinearRegression:
+    def __init__(self):
+        print(f"create instance for 'class {self.__class__.__name__}'")
