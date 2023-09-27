df10da2a3d14528b8fcbbe9d8998997f7a2ffc79
dukim <sajacall82@naver.com>
2023-09-27 21:14:08 +0900

    feat: fit 변수 초기화
    X의 각 행 마지막에 bias 계산을 위한 1값 추가
    계수(coef) 초기값 세팅

--- sajacaros_learn/Regression.py
+++ sajacaros_learn/Regression.py
@@ -3,6 +3,10 @@ from abc import abstractmethod, ABCMeta
 import numpy as np
 
 
+def mean_squared_error(y, y_hat):
+    return np.mean(np.square(y - y_hat))
+
+
 class NpModel(metaclass=ABCMeta):
     def __init__(self):
         print(f"create instance for 'class {self.__class__.__name__}'")
@@ -19,11 +23,15 @@ class NpModel(metaclass=ABCMeta):
 class NpLinearRegression(NpModel):
     def __init__(self):
         super().__init__()
+        self._coef = None
         self._lr = None
 
     def fit(self, X: np.ndarray, y: np.ndarray, lr: float = 0.1, it: int = 1000) -> None:
         self._lr = lr
-        print(X.shape, y.shape)
+        X_b = np.append(X, np.ones((X.shape[0], 1)), axis=1)
+        self._coef = np.random.rand(X_b.shape[1])  # features + b
+
+        print(X_b.shape, y.shape, self._coef)
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         pass
