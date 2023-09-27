73fc913e0b3a8453b13542c8c1834db6254048c9
dukim <sajacall82@naver.com>
2023-09-27 19:13:17 +0900

    feat: 최종 동작 형태 작성
    np_linear_reg = NpLinearRegression()
    np_linear_reg.fit(X, y, lr=0.1)
    y_hat = np_linear_reg.predict(t)
    error_report(y, y_hat)

---
+++ main.py
@@ -0,0 +1,10 @@
+def energy_predict():
+    # np_linear_reg = NpLinearRegression()
+    # np_linear_reg.fit(X, y, lr=0.1)
+    # y_hat = np_linear_reg.predict(t)
+    # error_report(y, y_hat)
+    pass
+
+
+if __name__ == '__main__':
+    energy_predict()
