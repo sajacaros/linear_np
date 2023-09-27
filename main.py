cd39cc41752c4a118b87a8ce7adf15fd3f23f511
dukim <sajacall82@naver.com>
2023-09-27 20:21:40 +0900

    feat: load dataset

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import pandas as pd
+
 from sajacaros_learn.Regression import NpLinearRegression
 
 
+def load_mission_dataset():
+    return pd.read_csv('data/mission_train.csv'), pd.read_csv('data/mission_test.csv')
+
+
 def energy_predict():
+    train_df, test_df = load_mission_dataset()
+
     np_linear_reg = NpLinearRegression()
     # np_linear_reg.fit(X, y, lr=0.1)
     # y_hat = np_linear_reg.predict(t)
