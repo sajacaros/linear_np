555e28c6cad28eaba51e7b0dc6298c11c8cd1c19
dukim <sajacall82@naver.com>
2023-09-27 20:14:28 +0900

    feat: NpModel abstract class
    1. def fit(self, X: np.ndarray, y: np.ndarray) -> None
    2. def predict(self, X: np.ndarray) -> np.ndarray

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def energy_predict():
     # np_linear_reg.fit(X, y, lr=0.1)
     # y_hat = np_linear_reg.predict(t)
     # error_report(y, y_hat)
-    pass
 
 
 if __name__ == '__main__':
--- sajacaros_learn/Regression.py
+++ sajacaros_learn/Regression.py
@@ -1,3 +1,27 @@
-class NpLinearRegression:
+from abc import abstractmethod, ABCMeta
+
+import numpy as np
+
+
+class NpModel(metaclass=ABCMeta):
     def __init__(self):
         print(f"create instance for 'class {self.__class__.__name__}'")
+
+    @abstractmethod
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        pass
+
+    @abstractmethod
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        pass
+
+
+class NpLinearRegression(NpModel):
+    def __init__(self):
+        super().__init__()
+
+    def predict(self):
+        pass
+
+    def fit(self):
+        pass
